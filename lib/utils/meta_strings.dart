84c471b19ea68d70fddeb8425a9a98b4678d2f3f
vaibhav72 <vaibhavanchan359@gmail.com>
2023-10-08 12:58:41 +0530

    feat: urls added

---
+++ lib/utils/meta_strings.dart
@@ -0,0 +1,57 @@
+class MetaStrings {
+  ///registration Urls
+  static const String userRegistrationUrl = '/user/new';
+  static const String verifyMobile = '/user/verifyMobile';
+
+  static const String generateOtp = '/auth/generateOtp';
+  static const String loginVerifyOTP = '/auth/verifyOtp';
+
+  static const String updateUserPhoto = '/user/photo';
+  static const String changeuserType = '/user/updateisglocker';
+  static const String createuserNotification = '/user-notification/new';
+  static const String updateNotification = '/user-notification/update';
+
+  //glocker
+
+  static const String glockerNew = '/glocker/new';
+  static const String glockerUpdate = '/glocker/update';
+  static const String updateGlockerPhoto = '/glocker/updateprofilephoto';
+  static const String updateGlockerCoverPhoto = '/glocker/updatecoverphoto';
+  static const String updateAadharFile = '/glocker/updateaadharphoto';
+  static const String updateAadhaar = 'glocker/updateaadhaar';
+  static const String updateVideKYC = '/glocker/updatevideokyc';
+  static const String getGlocker = '/glocker/';
+
+//bank
+
+  static const String updatebank = '/bank';
+  //gallery
+
+  static const String updateGalleryPhoto = '/gallery/photo';
+  static const String updateGalleryVideo = '/gallery/video';
+  static const String getGalleryVideo = 'gallery/video';
+  static const String getGalleryPhoto = 'gallery/photo';
+
+  //favorites
+  static const String getFavorites = '/favorite';
+  static const String updateFavorites = '/favorite/fav';
+
+  //rating
+  static const String updateUserRating = '/rating';
+  static const String updateGlockerRating = '/glokrating';
+
+  //money
+  static const String addFunds = '/money/addfund';
+  static const String withdrawFunds = '/money/withdraw';
+  static const String getAllTransactions = '/money';
+
+  //video call
+  static const String getVideoCall = '/videocall';
+  static const String endCall = '/videocall/disconnect';
+  static const String getAgoraToken = '/agora/token';
+
+  //bid
+  static const String bidNew = '/bid/new';
+  static const String bidList = '/bid/bidlist';
+  static const String acceptCall = '/bid/acceptcall';
+}
